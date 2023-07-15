838481c499433247157ae0caa7b775e16c937e97
shishir5 <shishir@doc32.in>
2023-07-15 10:13:17 +0530

    multi argument fucntions

---
+++ restParametersAndSpreadSyntax.js
@@ -0,0 +1,9 @@
+/*Math.max(arg1,arg2,arg3.......argN) - returns the greatest of the arguments
+	Object.assign(dest,src1, ... srcN) - copies properties from src1..N into dest
+	A function can be called any number of times no matter how it is defined.
+*/
+
+function sum(a,b){
+	return a + b;
+}
+console.log( sum(1,2,3,4,5));
